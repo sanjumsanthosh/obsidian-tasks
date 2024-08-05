834f76511a2afac6b8d343377dc540222940f089
sanjay <sanjay.m.santhosh+gitobs@gmail.com>
2024-08-05 21:53:14 +0530

    sync update

--- sync_script.sh
+++ sync_script.sh
@@ -5,4 +5,9 @@ pnpm install
 pnpm run build
 
 # cp main.js manifest.json styles.css resources/sample_vaults/Tasks-Demo/.obsidian/plugins/obsidian-tasks-plugin
+
+echo "Copying to $OBSIDIAN_PLUGIN_ROOT/obsidian-tasks-plugin ... "
+
 cp main.js manifest.json styles.css "$OBSIDIAN_PLUGIN_ROOT/obsidian-tasks-plugin"
+
+echo "Done."
